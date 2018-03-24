c9fb33305933c365b91a2170b7a8aaf25ae19906
Anderson Altissimo <ander.altissimo@gmail.com>
2018-03-24 10:07:26 -0300

    Conversores

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Anderson Altissimo on 3/22/18.
+//  Copyright © 2018 Anderson Altissimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+
+    @IBOutlet weak var btInit1: UIButton!
+    @IBOutlet weak var btInit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lvResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showNext(_ sender: UIButton) {
+        
+    }
+
+    @IBAction func convert(_ sender: UIButton) {
+        
+    }
+    
+}
+
